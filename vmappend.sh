4fb44bf6e3df04c344c35734b0c26fcaa7696b16
root <root@core-rtr.tmnkernels.lab>
2017-10-04 17:24:03 -0400

    Editing and working

--- vmappend.sh
+++ vmappend.sh
@@ -2,65 +2,76 @@
 
 # Author: Kent Graves
 # This script is used to append content to the end of a file.
+# During the deployment of a non-persistent VM.
 
 #-------- GLOBALS
 
-# Print debug output to $logfile for testing. 1 = basic output; 2 = basic + shell env
-debug=2    
+# Print debug output to $logfile for testing. 0 = no logging; 1 = logging
+debug=1 
+
+# Detect errors. 0 = no errors; 1 = error
+error=0
 
 # Destination of debug output if $debug -eq 1; make sure this is a sane location pls                 
 logfile="/tmp/debug.log"
 
 # File to update
-file='/etc/unbound/local.d/tmnkernels.com.conf'
+file='/etc/unbound/local.d/tmnkernels.lab.conf'
 
 
 # Updates appended to file
 content=(					
-'local-data-ptr: "172.17.0.160 admin1.tmnkernels.lab"'
-'local-data-ptr: "172.17.0.253 core-rtr.tmnkernels.lab"'
-'local-data-ptr: "219.13.189.1 core-rtr.tmnkernels.lab"'
+    'local-data-ptr: "172.17.0.160 admin1.tmnkernels.lab."'
+    'local-data-ptr: "172.17.0.253 core-rtr.tmnkernels.lab."'
+    'local-data-ptr: "219.13.189.1 core-rtr.tmnkernels.lab."'
+    'local-data-ptr: "219.13.189.173 www.linuxtools.net."'
 )
 
 #-------- FUNCTIONS
 
-bugLog{
-	msg=$1
-	[ $debug -gt 0 ] &&	/bin/echo "$msg" >> $logfile
+bugLog(){
+	msg="$1"
+	[ $debug -gt 0 ] &&	echo "$(date +%F\ %H:%M:%S) $msg" | tee -a ${logfile}
 }
 
-append{
-	msg=$1
-	file=$2
-	/bin/echo "$msg" >> $file
+append(){
+     msg="$1"
+     echo "$msg" | tee -a $file 2>> $logfile
+     [ $? -ne 0 ] && chkerr
 }
 
-chkerr{
-	[ $1 -ne 0 ] && log "ERROR: Error encountered"; exit 1
+chkerr(){
+	bugLog "ERROR: Error encountered"
+    exit 1
 }
 
-updateFile{
+updateFile(){
 	bugLog "[*] Attempting to update ${file} ..."
+    
 	count=0
-	for line in ${content[*]}; do
-		(( count++ ))
-		append $line $file
-		chkerr $?
+	while [ $count -le ${#content[@]} ]; do
+		append "${content[${count}]}"
+        (( count++ ))
 	done
-	
-	bugLog "[*] New update to ${file} :"
-	for line in $(/bin/tail -n $count $file); do
-		bugLog "$line"
-	done
+    bugLog "[*] Finished updating ${file}"
 }
 
-customActions{
+customActions(){
 	bugLog "[*] Stopping dnsmasq service"
-	systemctl stop dnsmasq
-	chkerr $?
+	systemctl stop dnsmasq 2>> $logfile
+    [ $? -ne 0 ] && chkerr
+
+    bugLog "[*] Disabling dnsmasq service"
+    systemctl disable dnsmasq 2>> $logfile
+    [ $? -ne 0 ] && chkerr
+
+    bugLog "[*] Masking dnsmasq service"
+    systemctl mask dnsmasq 2>> $logfile
+    [ $? -ne 0 ] && chkerr
+
 	bugLog "[*] Restarting unbound service"
-	systemctl restart unbound
-	chkerr $?
+	systemctl restart unbound 2>> $logfile
+    [ $? -ne 0 ] && chkerr
 }
 
 #-------- START SCRIPT
@@ -72,4 +83,4 @@ customActions
 bugLog "[*] Updates were successful."
 
 #remove script from system
-[ $debug -eq 0 ] && /bin/rm -f $0
+#[ $debug -eq 0 ] && /bin/rm -f $0
